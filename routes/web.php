010af7058744a783045d31e9f87766006fef9c7b
Victor <victorvelas.lst@gmail.com>
2026-01-27 23:49:06 -0500

    Add test route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return 'Hello world, this is a test route for Laravel application';
+});
